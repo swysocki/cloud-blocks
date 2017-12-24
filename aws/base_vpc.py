5bff09b4169fe0170d1a36037ef5b95a02355bbd
Scott Wysocki <swysocki@gmail.com>
2017-12-24 08:11:50 -0500

    ghetto test wrapper

--- aws/base_vpc.py
+++ aws/base_vpc.py
@@ -1,10 +1,34 @@
+""" Base VPC module
+
+This creates a list of troposphere resource object that can be 
+converted to a CloudFormation or combined with other resources
+to make a larger CFn Stack.
+
+resources created:
+- VPC object
+- a single route table
+- a single subnet per availiblity zone
+- subnet association for each subnet
+- internet gateway
+- default route
+"""
+
 from troposphere import Tags, Ref
 from troposphere.ec2 import VPC, RouteTable, Subnet, \
     SubnetRouteTableAssociation
 import utils
 
-def create_vpc(cidr_block, tag_app_name, resources):
-    vpc = VPC(
+def vpc(cidr_block, tag_app_name):
+    """ creates base VPC object
+
+    Params:
+        cidr_block: /16 subnet
+        tag_app_name: application name used to create AWS tag
+    Returns:
+        troposphere.ec2.VPC object
+    """
+
+    return VPC(
         'BaseVpc',
         CidrBlock=cidr_block,
         EnableDnsSupport=True,
@@ -14,56 +38,62 @@ def create_vpc(cidr_block, tag_app_name, resources):
             Name=tag_app_name+"-vpc"
         )
     )
-    resources.append(vpc)
-    return resources
 
-def route_table(tag_app_name, resources):
-    vpc_obj = [r for r in resources if type(r) == VPC]
-    resources.append(
-        RouteTable(
-            "RouteTable",
-            VpcId=Ref(vpc_obj),
-            Tags=Tags(
-                Name=tag_app_name+"-rt-1"
-            )
+def route_table(tag_app_name, vpc_obj):
+    """ creates a route table """
+    return RouteTable(
+        "RouteTable",
+        VpcId=Ref(vpc_obj),
+        Tags=Tags(
+            Name=tag_app_name+"-rt-1"
         )
     )
-    return resources
 
-def subnets(region, cidr_block, resources, tag_app_name=None):
+def subnets(region, cidr_block, vpc_obj, tag_app_name):
     azs = utils.get_availability_zones(region)
     subnet_list = utils.create_subnets(cidr_block, len(azs))
-    vpc = [r for r in resources if type(r) == VPC]
-    rt = [r for r in resources if type(r) == RouteTable]
+    subnet_resources = []
     for idx, sbnt in enumerate(subnet_list):
-        sbnt_obj = Subnet(
-            'Subnet'+str(idx+1),
-            CidrBlock=sbnt,
-            MapPublicIpOnLaunch=True,
-            VpcId=Ref(vpc),
-            Tags=Tags(
-                Name=tag_app_name+"-sbnt-"+str(idx+1)
+        subnet_resources.append(
+            Subnet(
+                'Subnet'+str(idx+1),
+                CidrBlock=sbnt,
+                MapPublicIpOnLaunch=True,
+                VpcId=Ref(vpc_obj),
+                Tags=Tags(
+                    Name=tag_app_name+"-sbnt-"+str(idx+1)
+                )
             )
         )
-        sbnt_assc_obj = SubnetRouteTableAssociation(
-            'SubnetAssociation'+str(idx+1),
-            SubnetId=Ref(sbnt_obj),
-            RouteTableId=Ref(rt)
-        )
-        resources.append(sbnt_obj)
-        resources.append(sbnt_assc_obj)
+    return subnet_resources
 
-def create(app_name, region, cidr):
+def subnet_assc(subnet_list, rt_obj):
+    subnet_associations = []
+    for idx, subnet in enumerate(subnet_list):
+        subnet_associations.append(
+            SubnetRouteTableAssociation(
+                'SubnetAssocation'+str(idx+1),
+                SubnetId=Ref(subnet),
+                RouteTableId=Ref(rt_obj)
+            )
+        )
+    return subnet_associations
+
+def build(app_name, region, cidr):
     resource_list = []
-    create_vpc(cidr, app_name, resource_list)
-    route_table(app_name, resource_list)
-    subnets(region, cidr, resource_list, app_name)
+    b_vpc = vpc(cidr, app_name)
+    resource_list.append(b_vpc)
+    
+    b_rt = route_table(app_name, b_vpc)
+    resource_list.append(b_rt)
+
+    b_subnets = subnets(region, cidr, b_vpc, app_name)
+    for sub in b_subnets:
+        resource_list.append(sub)
+
+    b_sbnt_assc = subnet_assc(b_subnets, b_rt)
+    for sb_asc in b_sbnt_assc:
+        resource_list.append(sb_asc)
 
     return resource_list
 
-if __name__ == "__main__":
-    APP = 'my-app'
-    REGION = "us-east-1"
-    CIDR = "10.1.0.0/16"
-    RES = create(APP, REGION, CIDR)
-    print(RES)
