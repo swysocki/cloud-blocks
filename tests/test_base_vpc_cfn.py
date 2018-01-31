0250f3b9d0d9ad130c33ce7240cdc2b1c2eef7b8
Scott Wysocki <swysocki@gmail.com>
2018-01-31 16:34:06 -0500

    created template yaml unittest

--- tests/test_base_vpc_cfn.py
+++ tests/test_base_vpc_cfn.py
@@ -1,4 +1,4 @@
-
+import unittest
 import tempfile
 import sys
 import os
@@ -9,13 +9,21 @@ sys.path.append(os.path.abspath('..'))
 from troposphere import Template
 from aws import base_vpc
 
+class TestBaseVPCCFN(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.TEMPLATE = Template()
+        cls.VPC = base_vpc.build('testing-app,', 'us-east-1', '1.0.0.0/16')
+    
+    def test_vpc_cfn(self):
+        for resource in self.VPC:
+            self.TEMPLATE.add_resource(resource)
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as fp:
+            fp.write(self.TEMPLATE.to_yaml())
+            fp.close()
+            ret = subprocess.run(['cfn-lint', 'validate', fp.name], check=True)
+            self.assertEqual(ret.returncode, 0)
+            os.unlink(fp.name)
+
 if __name__ == "__main__":
-    TEMPLATE = Template()
-    VPC = base_vpc.build('testing-app', 'us-east-1', '1.0.0.0/16')
-    for resource in VPC:
-        TEMPLATE.add_resource(resource)
-    with tempfile.NamedTemporaryFile(mode="w", delete=False) as fp:
-        fp.write(TEMPLATE.to_yaml())
-        fp.close()
-        subprocess.run(['cfn-lint', 'validate', fp.name], shell=True)
-        os.unlink(fp.name)
+    unittest.main()
